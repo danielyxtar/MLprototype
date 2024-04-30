c0dab6fc29c2427a148cb1a3e142fae4abd798c6
danielyxtar <167959819+danielyxtar@users.noreply.github.com>
2024-04-30 11:48:33 +0800

    Update MLprototype.py

--- MLprototype.py
+++ MLprototype.py
@@ -2,20 +2,19 @@ import streamlit as st
 import pandas as pd
 
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-    for uploaded_file in uploaded_files:
-        bytes_data = uploaded_file.read()
-        st.write("filename:", uploaded_file.name)
-        st.write(bytes_data)
-        base64_pdf = base64.b64encode(bytes_data.read()).decode('utf-8')
-        pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'   
-        st.markdown(pdf_display, unsafe_allow_html=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+    base64_pdf = base64.b64encode(bytes_data.read()).decode('utf-8')
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'   
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
     #Faire une fonction avec le display
-    with open(bytes_data,"rb") as f:
-      base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+with open(bytes_data,"rb") as f:
+    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     
     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'   
     st.markdown(pdf_display, unsafe_allow_html=True)
 
     st.write('Statistics')
-    st.image('Statistics on Icebreakers.png')
