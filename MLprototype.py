4f0f696e0d56eb0c5b3acb2c645d7f6a583ad208
danielyxtar <167959819+danielyxtar@users.noreply.github.com>
2024-05-04 13:50:04 +0800

    Update MLprototype.py

--- MLprototype.py
+++ MLprototype.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import pandas as pd
+from st_pages import Page, show_pages, add_page_title
+
+show_pages(
+    [
+        Page("MLprototype.py", "Home"),
+        Page("other_pages/testJ.py", "Prediction"),
+    ]
+)
 
 st.set_page_config(
     page_title="Home"
