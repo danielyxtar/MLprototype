95b7fb240d7afe05e12fc71bf8bc4cbf0e16b945
danielyxtar <167959819+danielyxtar@users.noreply.github.com>
2024-05-04 20:39:07 +0800

    Update MLprototype.py

--- MLprototype.py
+++ MLprototype.py
@@ -18,4 +18,4 @@ col1, col2, col3 = st.columns([1,2,1])
 with col2:
     st.image('logo.png', width=180)
 
-st.write("Start Analysing the severity of patients with HCV")
+st.write("Start Analysing the severity of patients with HCV"; font-size: 42px)
