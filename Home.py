017a278d7f7dd663080ddd5dbdd000ef03d09bf6
danielyxtar <167959819+danielyxtar@users.noreply.github.com>
2024-05-04 13:24:00 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,20 +2,11 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(
-    page_title="PrototypeJ"
+    page_title="Home"
 )
 
 st.title('Welcome to Rhine Pharmeceuticals')
 
-st.write('CSV File Upload')
-
-uploaded_file = st.file_uploader("Drag and Drop or Click to Upload a CSV file", type=['csv'])
-
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    
-    st.write(df)
-else:
-    st.write("Please upload a CSV file.")
+st.image('logo.png', width=180)
 
 
